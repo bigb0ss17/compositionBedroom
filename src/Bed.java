31db6df330b45d8148b9048a90e001e5ea046975
vpham7 <vincentpham1720@gmail.com>
2024-09-13 16:40:41 -0400

    The mitochondria is the powerhouse of the cell.

--- src/Bed.java
+++ src/Bed.java
@@ -1,2 +1,26 @@
 public class Bed {
+
+    private String name;
+    private String style;
+    private String size;
+
+    public Bed(){
+        this("My Bedroom", "Modern", "Full");
+
+    }
+
+    public Bed(String name, String style, String size){
+        this.name = name;
+        this.style = style;
+        this.size = size;
+    }
+    //getters
+
+    public String getName(){return name;}
+    public String getStyle(){return style;}
+    public String getSize(){return size;}
+
+    public void make(){
+        System.out.println("From Bed class: make() method");
+    }
 }
--- src/Bedroom.java
+++ src/Bedroom.java
@@ -12,5 +12,47 @@ public class Bedroom {
     Bed bed;
     Lamp lamp;
 
+    public Bedroom(){
+
+    }
+
+    public Bedroom(String name, Wall w1, Wall w2, Wall w3, Wall w4, Ceiling ceiling){
+        this.name = name;
+        this.w1 = w1;
+        this.w2 = w2;
+        this.w3 = w3;
+        this.w4 = w4;
+
+
+        this.ceil = ceiling;
+    }
+
+    //setters
+    public void Name(String name){this.name = name;}
+
+
+
+    //getters
+    public String getName(){return name;}
+    public Wall getW1(){return w1;}
+    public Wall getW2(){return w2;}
+    public Wall getW3(){return w3;}
+    public Wall getW4(){return w4;}
+
+    public void makeBed(){
+        System.out.println("Bedroom class ----> calling makeBed() method.");
+        //call make() method you declared in the Bedroom class
+        this.make();
+
+        //call make() method you created in the Bed class
+        bed.make();
+
+
+    }
+
+    public void make(){
+        System.out.println("Bedroom class ----> calling make() method.");
+    }
+
 
 }
